b4f370e32ae6d4830690a93fae014478dfee2696
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-09-21 16:54:08 -0300

    requisitos 2, 3, 4 e 5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Pages/Login';
+import Provider from './Context/Provider';
 
 function App() {
   return (
-    <div>
+    <Provider>
       <Login />
-    </div>
+    </Provider>
   );
 }
 
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../Context/Context';
+
+const MIN_LENGTH = 6;
 
 function Login() {
+  const { user, setUser } = useContext(Context);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  //   function validateEmail(email) {
+  //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //     return re.test(String(email).toLowerCase());
+  //   }
+
+  const validate = () => {
+    const { email, password } = user;
+    if (email.includes('@') && email.includes('.com') && password.length > MIN_LENGTH) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div>
       <form>
         <label htmlFor="email-input">
           <input
+            value={ user.email }
+            onChange={ (e) => handleChange(e) }
             data-testid="email-input"
             type="text"
-            name=""
+            name="email"
             id="email-input"
           />
         </label>
         <label htmlFor="password-input">
           <input
+            value={ user.password }
+            onChange={ (e) => handleChange(e) }
             data-testid="password-input"
             type="text"
-            name=""
+            name="password"
             id="password-input"
           />
         </label>
         <button
+          disabled={ validate() }
           data-testid="login-submit-btn"
           type="button"
         >
