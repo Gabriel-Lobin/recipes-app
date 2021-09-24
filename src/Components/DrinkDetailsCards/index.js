30894ecbf4fa48e2e1a42f7d8e18d44bd255f3d3
Dororops <leeurw@hotmail.com>
2021-09-24 16:44:42 -0300

    Arrumando lint

--- src/Components/DrinkDetailsCards/index.js
+++ src/Components/DrinkDetailsCards/index.js
@@ -3,13 +3,14 @@ import Context from '../../Context/Context';
 
 function DrinkDetailsCards() {
   const { drinkRecomendations } = useContext(Context);
+  const MAX_LENGTH = 6;
 
   return (
     <>
       <h2>Recomended Meals</h2>
       {
         drinkRecomendations.map((meal, index) => {
-          if (index < 6) {
+          if (index < MAX_LENGTH) {
             return (
               <div key={ index } data-testid={ `${index}-recomendation-card` }>
                 <h4 data-testid={ `${index}-recomendation-title` }>
--- src/Components/MealDetailsCards/index.js
+++ src/Components/MealDetailsCards/index.js
@@ -3,13 +3,14 @@ import Context from '../../Context/Context';
 
 function MealDetailsCards() {
   const { mealRecomendations } = useContext(Context);
+  const MAX_LENGTH = 6;
 
   return (
     <>
       <h2>Recomended Drinks</h2>
       {
         mealRecomendations.map((drink, index) => {
-          if (index < 6) {
+          if (index < MAX_LENGTH) {
             return (
               <div key={ index } data-testid={ `${index}-recomendation-card` }>
                 <h4 data-testid={ `${index}-recomendation-title` }>
