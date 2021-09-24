cf10b7dc042831f190622f266dc8bb4d77a02356
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-09-24 16:40:15 -0300

    to pull main

--- src/Pages/MakingDrinks/index.js
+++ src/Pages/MakingDrinks/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MakingDrinks() {
   return (
     <div>
-      hello
+      drinks
     </div>
   );
 }
--- src/Pages/MakingMeals/index.js
+++ src/Pages/MakingMeals/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../../Components/Footer/index';
 
 function MakingMeals() {
+  const meal = [];
+  useEffect(() => {
+    async function fetchmeal() {
+      await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771')
+        .then((result) => result.json())
+        .then(({ meals }) => meal.push(meals));
+    }
+    fetchmeal();
+  }, []);
+
   return (
-    <div>
-      hello
+    <div key={ meal.idMeal }>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        x
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        S2
+      </button>
+      <h1 data-testid="recipe-title">{ meal.strMeal }</h1>
+      <img data-testid="recipe-photo" src={ meal.strMealThumb } alt="food" />
+      <p data-testid="recipe-category">{meal.strCategory}</p>
+      <p data-testid="instructions">{meal.strInstructions}</p>
+      <p data-testid={ `${meal.idMeal}-ingredient-step` }>provisório</p>
+      <button
+        data-testid="finish-recipe-btn"
+        type="button"
+      >
+        terminado
+      </button>
       <Footer />
     </div>
   );
