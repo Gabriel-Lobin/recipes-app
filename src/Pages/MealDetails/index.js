b0a2f13e9491fcd9cb045e5ab20ea5e00ca7b54e
Dororops <leeurw@hotmail.com>
2021-09-27 16:24:24 -0300

    Group 17 - req 39 e 40

--- src/Pages/MealDetails/index.js
+++ src/Pages/MealDetails/index.js
@@ -17,7 +17,7 @@ function MealDetails({ match: { params: { id } }, location: { pathname } }) {
 
   const [load, setLoad] = useState(false);
   // const LOAD_TIMER = 1800;
-  // ab
+  // a
   const goTo = useHistory();
 
   const mealToLocalStorage = {
