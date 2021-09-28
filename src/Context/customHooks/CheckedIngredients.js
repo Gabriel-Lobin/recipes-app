eb56dab6da58a34d0fa21e39f992ee351da7ef1f
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-09-28 19:34:44 -0300

    [in progress] lint temporario

--- src/Context/customHooks/CheckedIngredients.js
+++ src/Context/customHooks/CheckedIngredients.js
@@ -1,12 +1,12 @@
 import { useContext, useEffect } from 'react';
 import Context from '../Context';
 
-const organizeDrinkState = (id, inProgressRecipes, inProgressRecipes, setInProgressRecipe) => {
-
+const organizeDrinkState = (id, inProgressRecipes, setInProgressRecipe) => {
+  console.log(id, inProgressRecipes, setInProgressRecipe);
 };
 
-const organizeMealState = (id, inProgressRecipes, inProgressRecipes, setInProgressRecipe) => {
-
+const organizeMealState = (id, inProgressRecipes, setInProgressRecipe) => {
+  console.log(id, inProgressRecipes, setInProgressRecipe);
 };
 
 export default LocalStorageProgress = (typeState, id) => {
@@ -15,9 +15,9 @@ export default LocalStorageProgress = (typeState, id) => {
     const StorageProgress = localStorage.getItem('inProgressRecipes');
     if (StorageProgress !== null) {
       if (typeState === 'drink') {
-        organizeDrinkState(id, inProgressRecipes, inProgressRecipes, setInProgressRecipe);
+        organizeDrinkState(id, inProgressRecipes, setInProgressRecipe);
       } else if (typeState === 'meal') {
-        organizeMealState(id, inProgressRecipes, inProgressRecipes, setInProgressRecipe);
+        organizeMealState(id, inProgressRecipes, setInProgressRecipe);
       }
     }
   }, []);
